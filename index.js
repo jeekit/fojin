fb0663eace58d354db3067e9e314116603ad60e9
jeekit <631321896@qq.com>
2018-10-09 16:39:04 +0800

    增加vue

--- index.js
+++ index.js
@@ -2,11 +2,87 @@
 * @Author: jeekit
 * @Date:   2018-10-09 15:55:01
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-10-09 15:55:39
+* @Last Modified time: 2018-10-09 16:32:55
 */
 var app = new Vue({
   el: '#header',
   data: {
-    message: 'Hello Vue!'
+    message: 'This is the title!'
+  }
+})
+
+// demo data
+var data = {
+  name: 'My Tree',
+  children: [
+    { name: 'hello' },
+    { name: 'wat' },
+    {
+      name: 'child folder',
+      children: [
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        },
+        { name: 'hello' },
+        { name: 'wat' },
+        {
+          name: 'child folder',
+          children: [
+            { name: 'hello' },
+            { name: 'wat' }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+// define the item component
+Vue.component('item', {
+  template: '#leftNav-template',
+  props: {
+    model: Object
+  },
+  data: function () {
+    return {
+      open: false
+    }
+  },
+  computed: {
+    isFolder: function () {
+      return this.model.children &&
+        this.model.children.length
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.isFolder) {
+        this.open = !this.open
+      }
+    },
+    changeType: function () {
+      if (!this.isFolder) {
+        Vue.set(this.model, 'children', [])
+        this.addChild()
+        this.open = true
+      }
+    },
+    addChild: function () {
+      this.model.children.push({
+        name: 'new stuff'
+      })
+    }
+  }
+})
+
+// boot up the demo
+var leftNav = new Vue({
+  el: '#leftNav',
+  data: {
+    treeData: data
   }
 })
