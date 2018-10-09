4b45a9b7ebe573f792720d64268ad2c688aa75ad
jeekit <631321896@qq.com>
2018-10-09 15:56:36 +0800

    2018

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+* @Author: jeekit
+* @Date:   2018-10-09 15:55:01
+* @Last Modified by:   Marte
+* @Last Modified time: 2018-10-09 15:55:39
+*/
+var app = new Vue({
+  el: '#header',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
